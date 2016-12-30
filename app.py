d1ebf2a04fb4900e5abe9ea03c4858d4ab151852
tnkarthik <tnkarthik@alumni.stanford.edu>
2016-12-30 19:44:41 +0000

    MVP APP

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template, request, redirect
+from plot_data import get_data
+from plot_df import plot_df
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
-@app.route('/index')
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    #return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        
+        result = request.form
+        #colnames.remove('ticker')
+        ticker = result['ticker']
+        
+        colnames = [str(result[x]) for x in result.keys() if x != 'ticker']
+
+        df  = get_data( ticker = ticker, selected_cols = colnames)
+        
+        plot_df(df)
+        
+        return render_template('stock_price.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(host = '0.0.0.0',port=33507)
